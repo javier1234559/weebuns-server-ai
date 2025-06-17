ac289e6af075cf3335af3e6e66cdf1b4f68a003e
javier <nhat1234559@gmail.com>
2025-06-18 01:18:58 +0700

    fix: :sparkles: update domain for email resend

--- src/common/mail/mail.service.ts
+++ src/common/mail/mail.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
 import { Resend } from 'resend';
-
 import { resetPasswordTemplate } from 'src/common/mail/templates/reset-password.template';
 
 @Injectable()
 export class MailService {
+  private readonly logger = new Logger(MailService.name);
   private resend: Resend;
+  private readonly fromEmail = 'no-reply@waebuns.com';
+  private readonly fromName = 'Weebuns Learning';
 
   constructor(private configService: ConfigService) {
     this.resend = new Resend(this.configService.get('RESEND_API_KEY'));
@@ -15,14 +16,19 @@ export class MailService {
 
   async sendPasswordResetEmail(email: string, resetCode: string) {
     try {
-      await this.resend.emails.send({
-        from: 'Weebuns Learning <no-reply@backend.waebuns.com>',
+      this.logger.log(`Sending password reset email to ${email}`);
+
+      console.log(this.configService.get('RESEND_API_KEY'));
+      const res = await this.resend.emails.send({
+        from: `${this.fromName} <${this.fromEmail}>`,
         to: email,
         subject: 'Password Reset Code',
         html: resetPasswordTemplate(resetCode),
       });
+      console.log(JSON.stringify(res, null, 2));
+      return res;
     } catch (error) {
-      console.error('Failed to send email:', error);
+      this.logger.error('Failed to send email:', error);
       throw new Error('Failed to send reset code email');
     }
   }
